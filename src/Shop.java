cd2f45f3fb43713fe87df466fa36ee51fba88393
Konrad Hołówko <konrad77777@gmail.com>
2025-05-26 18:55:12 +0200

    Added shop

---
+++ src/Shop.java
@@ -0,0 +1,11 @@
+public class Shop {
+    public void buyFromShop(int choice){
+        System.out.println("Welcome to the SHOP\nWhat you want to buy?\n" +
+                "1. Healing potion\n" +
+                "2. Bow\n" +
+                "3. Arrows (5)\n" +
+                "4. Leave shop");
+
+
+    }
+}
